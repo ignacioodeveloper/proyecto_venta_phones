1824afb1bcdddc2cd768cef773286eecab423b3c
luxossj <luisignaciocontacto@gmail.com>
2021-07-07 18:36:52 -0400

    login terminado

--- ventaPhones/apps/Usuario/views.py
+++ ventaPhones/apps/Usuario/views.py
@@ -1,19 +1,3 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
-from .forms import RegistroForm
-
 
 # Create your views here.
-
-class RegistroUsuario(CreateView):
-    model = User
-    template_name = 'Usuario/usuario_form.html'
-    form_class = RegistroForm
-    success_url = reverse_lazy('home')
-    
-class UserList(ListView):
-    model = User
-    template_name = 'Usuario/listar_usuarios.html'
